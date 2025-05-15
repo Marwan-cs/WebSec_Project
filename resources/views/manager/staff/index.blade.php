ca121481e9afb4271f3e5e3d29c2f9cfde72b842
Ahmed_Eldesouky <mido.ddm2004@gmail.com>
2025-05-16 00:29:15 +0300

    manage staff dashboard

---
+++ resources/views/manager/staff/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5 class="mb-0">{{ __('Manage Staff') }}</h5>
+                    <a href="#" class="btn btn-primary disabled">Add Staff</a>
+                </div>
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>{{ __('Name') }}</th>
+                                    <th>{{ __('Email') }}</th>
+                                    <th>{{ __('Role') }}</th>
+                                    <th>{{ __('Actions') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($staff as $user)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->roles->pluck('name')->join(', ') }}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-info btn-sm disabled">View</a>
+                                            <a href="#" class="btn btn-primary btn-sm disabled">Edit</a>
+                                            <form action="#" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm disabled">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center">{{ __('No staff found.') }}</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection 
